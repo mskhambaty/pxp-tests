1b33ed9dd14b71d64a5bfbe67fa3a1be92569e5b
sameerzahidk <140701314+sameerzahidk@users.noreply.github.com>
2025-09-09 18:27:01 +0500

    change

--- tests/specs/collectibles.spec.ts
+++ tests/specs/collectibles.spec.ts
@@ -8,6 +8,12 @@ import { CollectiblePage } from '../../pages/Collectible.page';
 
 test.describe('Collectibles Management', () => {
   test('create and update paid collectible', async ({ page }) => {
+    // Skip this test if running on a mobile viewport
+    const viewport = page.viewportSize && page.viewportSize();
+    // Typical mobile max width is 767px
+    if (viewport && viewport.width <= 767) {
+      test.skip();
+    }
     console.log('Test: Starting collectibles test');
     const dashboard = new OrganizerDashboardPage(page);
     const collectibles = new CollectiblePage(page);
@@ -24,12 +30,12 @@ test.describe('Collectibles Management', () => {
     await page.waitForTimeout(3000);
     console.log('Test: dashboard.openNavTab("collectibles") completed');
     let collectibleName = 'Test collectible ' + Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 8);
-    // await collectibles.createPaidCollectible({
-    //   name: collectibleName,
-    //   price: 100,
-    //   metricAmount: 100,
-    //   rewardTitle: 'Thankyou letter',
-    // });
+    await collectibles.createPaidCollectible({
+      name: collectibleName,
+      price: 100,
+      metricAmount: 100,
+      rewardTitle: 'Thankyou letter',
+    });
     await collectibles.editPaidCollectibleAmount(collectibleName, 200, 200);
   });
 });
